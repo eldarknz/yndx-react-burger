faebc054c6845f81391ad15e0d98a6df8bcaa825
Eldar <eldarknz@gmail.com>
2022-03-13 23:04:29 +0300

    sptrin-2/step-2

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/globals.css';
 import App from './components/App/App';
+
+import { compose, createStore, applyMiddleware } from 'redux';
+//import { composeWithDevTools } from 'redux-devtools-extension';
+//import { Provider } from 'react-redux';
+import { rootReducer } from './services/reducers';
+import thunk from 'redux-thunk';
 //import reportWebVitals from './reportWebVitals';
+
+//const composeEnhancers =
+//  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//    : compose;
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <React.StrictMode>
