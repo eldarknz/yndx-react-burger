9b50665c6ebf48a44b48523a5bab5cd870ae9876
eldarknz <eldarknz@gmail.com>
2022-03-23 10:24:20 +0300

    Инициализация и работа с хранилищем вынесена в services/store.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { compose, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
 import { Provider } from 'react-redux';
-import { rootReducer } from 'services/reducers/rootReducer';
+import store from 'services/store';
 
 import App from './components/App/App';
 import './styles/globals.css';
 
 import reportWebVitals from './reportWebVitals';
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/services/store.js
@@ -0,0 +1,16 @@
+// Код файла store.js
+import { compose, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { rootReducer } from './reducers/rootReducer';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
+
+export default store;
