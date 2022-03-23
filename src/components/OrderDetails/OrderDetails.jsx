4c420fc22dcd41f6e5504d40ecb2bef36f966343
eldarknz <eldarknz@gmail.com>
2022-03-23 12:35:17 +0300

    добавил состояние загрузки и вывода ошибки, если запрос номера заказа оказался неудачным

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -5,17 +5,28 @@ import { useSelector } from 'react-redux';
 import styles from "./OrderDetails.module.css";
 
 const OrderDetails = () => {
-    const orderNumber = useSelector(store => store.app.orderNumber);
+    const { orderNumber, orderNumberRequest, orderNumberFailed } = useSelector(store => store.app);
 
     return (
         <div className={cn(styles.orderBlock, "pb-15")}>
-            <div className={cn(styles.title, "pb-8")}>
-                <h1 className="text text_type_digits-large">{orderNumber}</h1>
-            </div>
-            <p className="text text_type_main-medium">идентификатор заказа</p>
-            <div className={cn(styles.icon, "pt-15 pb-15")}></div>
-            <p className="text text_type_main-default pb-2">Ваш заказ начали готовить</p>
-            <p className="text text_type_main-default text_color_inactive">Дождитесь готовности на орбитальной станции</p>
+            { orderNumberFailed && (
+                <>
+                    <p className="text text_type_main-default pb-3">Не удалось офоромить заказ</p>
+                    <p className="text text_type_main-default pb-3">Попробуйте это сделать позже</p>
+                </>
+            )}
+            { orderNumberRequest && <p className="text text_type_main-default pb-3">Загрузка...</p> }
+            { orderNumber && !orderNumberRequest && !orderNumberFailed && (
+                <>
+                    <div className={cn(styles.title, "pb-8")}>
+                        <h1 className="text text_type_digits-large">{orderNumber}</h1>
+                    </div>
+                    <p className="text text_type_main-medium">идентификатор заказа</p>
+                    <div className={cn(styles.icon, "pt-15 pb-15")}></div>
+                    <p className="text text_type_main-default pb-2">Ваш заказ начали готовить</p>
+                    <p className="text text_type_main-default text_color_inactive">Дождитесь готовности на орбитальной станции</p>
+                </>
+            )}
         </div>
     );
 };
